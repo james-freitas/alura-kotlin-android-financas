cf28dfa5bf663af49422ce8b37e1180671644515
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-07-03 16:11:21 -0300

    Creating model classes and using properties

---
+++ app/src/main/java/br/com/alura/financask/model/Transacao.kt
@@ -0,0 +1,11 @@
+package br.com.alura.financask.model
+
+import java.math.BigDecimal
+import java.util.Calendar
+
+class Transacao(val valor: BigDecimal,
+                val categoria: String,
+                val data: Calendar) {
+
+
+}
