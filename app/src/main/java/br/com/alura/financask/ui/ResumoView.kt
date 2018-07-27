6c1465506cca387a48beb0d73b11037bd7f795be
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-07-27 16:29:43 -0300

    Code secuirty with null safety

--- app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
+++ app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
@@ -10,16 +10,16 @@ import br.com.alura.financask.model.Transacao
 import kotlinx.android.synthetic.main.resumo_card.view.*
 import java.math.BigDecimal
 
-class ResumoView(private val context: Context,
-                 private val view: View,
+class ResumoView(context: Context,
+                 private val view: View?,
                  transacoes: List<Transacao>) {
 
-    private val resumo : Resumo = Resumo(transacoes)
+    private val resumo: Resumo = Resumo(transacoes)
 
     private val corReceita = ContextCompat.getColor(context, R.color.receita)
     private val corDespesa = ContextCompat.getColor(context, R.color.despesa)
 
-    fun atualiza(){
+    fun atualiza() {
         adicionaReceita();
         adicionaDespesa();
         adicionaTotal();
@@ -27,26 +27,33 @@ class ResumoView(private val context: Context,
 
     private fun adicionaReceita() {
         val totalReceita = resumo.receita
-        with(view.resumo_card_receita){
-            setTextColor(corReceita)
-            text = totalReceita.formataParaBrasileiro()
+        view?.let {
+            with(it.resumo_card_receita) {
+                setTextColor(corReceita)
+                text = totalReceita.formataParaBrasileiro()
+            }
         }
+
     }
 
     private fun adicionaDespesa() {
         val totalDespesa = resumo.despesa
-        with(view.resumo_card_despesa){
-            setTextColor(corDespesa)
-            text = totalDespesa.formataParaBrasileiro()
+        view?.let {
+            with(it.resumo_card_despesa) {
+                setTextColor(corDespesa)
+                text = totalDespesa.formataParaBrasileiro()
+            }
         }
     }
 
     private fun adicionaTotal() {
         val total = resumo.total
         val cor = corPor(total)
-        with(view.resumo_card_total){
-            setTextColor(cor)
-            text = total.formataParaBrasileiro()
+        view?.let {
+            with(it.resumo_card_total) {
+                setTextColor(cor)
+                text = total.formataParaBrasileiro()
+            }
         }
     }
 
